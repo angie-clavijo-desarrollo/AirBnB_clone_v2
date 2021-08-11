58c9a015df5da262b3108b13325a38772601151b
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-11 14:46:52 -0500

    place user

--- models/place.py
+++ models/place.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.review import Review
-from sqlalchemy import Column
-from sqlalchemy import Float
-from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import Table
+from sqlalchemy.sql import base
+from sqlalchemy.sql.schema import ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from models.state import State
 from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
+
+    __tablename__ = 'places'
+    city_id = Column(String(60), nullable=False, )
     user_id = ""
     name = ""
     description = ""

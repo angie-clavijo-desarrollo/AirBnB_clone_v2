b61807e4a5088959d9874c12d226637cd379b899
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-26 18:59:41 -0500

    function pack with library Fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Fabric for create a local pack with web static"""
+from fabric.api import *
+from os import path
+from datetime import datetime
+
+
+def do_pack():
+    """ Function pack"""
+    x = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    if not path.exists("./versions"):
+        local("mkdir versions")
+
+    format_file = "versions/web_static_{}.tgz".format(x)
+    try:
+        local("tar -cvzf {} web_static").format(format_file)
+        return(format_file)
+    except Exception as exception:
+        return(None)

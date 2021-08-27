8c6d6be7ef888abf45360558602616580a69fc3e
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-27 16:24:16 -0500

    correction permission sudo and verify if created files correct

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,20 +2,17 @@
 """ Fabric for create a
     local pack with web static """
 from fabric.api import local
-from os import path
 from datetime import datetime
 
 
 def do_pack():
     """ Function pack """
     x = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    if not path.exists("./versions"):
-        local("sudo mkdir -p versions")
+    local("mkdir -p versions")
 
     format_file = "versions/web_static_{}.tgz".format(x)
     try:
-        local("sudo tar -cvzf {} web_static").format(format_file)
+        local("tar -cvzf {} web_static").format(format_file)
         return(format_file)
     except Exception as exception:
         return(None)

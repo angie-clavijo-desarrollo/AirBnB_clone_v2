ff1b6fdb603ce7d1407146e0c10c29928736444d
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-08-28 22:23:40 -0500

    update file for convert at files tgz y format datetime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,9 @@ def do_pack():
     """ Function pack """
     x = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-
     format_file = "versions/web_static_{}.tgz".format(x)
     try:
-        local("tar -cvzf {} web_static").format(format_file)
+        local("tar -czvf " + format_file + " web_static")
         return(format_file)
     except Exception as exception:
         return(None)

3da2c3a60c416985af3aa2640bf9861263ef259f
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-09-11 15:32:48 -0500

    PYTHON AND API FLASK files with routes different and enrouting with file html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def path_number(n):
 @app.route('/number_template/<int:n>')
 def path_number_temaple(n):
     return "Number: {}".format(n)
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':

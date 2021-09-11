4eff0b7fbc82ac40fb8b79e73c1884db08d9efde
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-09-11 01:10:12 -0500

    PYTHON AND API FLASK files hello world

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Flask hello world and other path and specify port and host"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def path_hbnbn():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def path_c(text="value"):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>')
+def path_python(text="is cool"):
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<n>')
+def path_number(n="value"):
+    if n is int:
+        return "{}  is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
